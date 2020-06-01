f04167807dc8146b3c4cccad8cacd73896b1b438
Stephen Byers <sabyers1@gmail.com>
2020-06-01 17:06:56 -0400

    Added more vector pair examples

--- quizApp/BoundryPairs.cpp
+++ quizApp/BoundryPairs.cpp
@@ -3,6 +3,8 @@
  */
 #include <iostream>
 #include <set>
+#include <vector>
+
 using namespace std;
 
  // Function for lower bound
@@ -25,6 +27,46 @@ using namespace std;
             << (*up).first << ", " << (*up).second <<"}" << endl;
      }
 
+
+typedef pair<int, int> pairs;
+
+void display(const set<pairs>& s){
+    bool found = false;
+
+    for(auto const &x : s){
+        found = true;
+        cout << "(" << x.first << ", " << x.second << ")" << " ";
+    }
+    if (not found){
+        cout << "No valid pair found" << endl;
+    }
+    cout << endl;
+}
+
+void testDisplay(){
+    vector<int> vect{ 2, 3, 1, 6, 8, 8, 10, 2 };
+    set <pairs> s;
+    
+    /* Make pairs of all even numbers present in array */
+    for (int i = 0; i < vect.size(); i++){
+        for (int j=i; j<vect.size(); j++) {
+            // check for even numbers
+            if (vect[i] %2 == 0 && vect[j] % 2 == 0 ){
+                // make pairs of even numbers
+                pairs x = make_pair(vect[i],vect[j]);
+                s.insert(x);  // insert into set.
+            }
+        }
+    }
+
+    cout << "Using set: { ";
+    std::copy(vect.begin(),vect.end(),ostream_iterator <int> (std::cout, " "));
+    cout << "} list pairs of even numbers:"<< endl;
+    display(s);  // display sets
+    s.clear();   // clear the set.
+
+}
+
 int main(){
 
     // Given sorted vector of Pairs
@@ -40,6 +82,8 @@ int main(){
     findLowerBound(s, p);
 
     findUpperBound(s, p);
+
+    testDisplay();
 
     return 0;
 }
